c0ba041a7d88aedea7e6bdf5ba48db505ced3bd9
HuyJokezy <nqhuy1811@gmail.com>
2017-07-17 17:52:04 +0700

    fbmsg\index.js

---
+++ fbmsg/index.js
@@ -0,0 +1,13 @@
+'use strict';
+exports.handler = function (req, res) {
+	// Facebook Webhook setup
+	if (req.method === 'GET') {
+		if (req.query['hub.mode'] === 'subcribe' && req.query['hub.verify_token'] === 'iambobbeepboop') {
+			res.status(200).send(req.query['hub.challenge']);
+		} else {
+			res.status(403).send('Invalid Request');
+		}
+	} else if (req.method === 'POST') {
+		res.status(200).json({});
+	}
+}
