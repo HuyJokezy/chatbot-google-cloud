c7a1a0bcf50ec753af53277e53863c0a50d405de
HuyJokezy <nqhuy1811@gmail.com>
2017-07-18 09:32:20 +0700

    fbmsg\index.js

--- fbmsg/index.js
+++ fbmsg/index.js
@@ -4,7 +4,7 @@ exports.handler = function (req, res) {
 	// Facebook Webhook setup
 	if (req.method === 'GET') {
 		console.log('Incoming request:\n' + JSON.stringify(req));
-		console.log('Body:\n' + JSON.stringify(req));
+		console.log('Body:\n' + JSON.stringify(req.body));
 		if (req.query['hub.mode'] === 'subcribe' && req.query['hub.verify_token'] === 'iambobbeepboop') {
 			res.status(200).send(req.query['hub.challenge']);
 		} else {
