c430443d8605e62f95efc22a487617d26629f11a
HuyJokezy <nqhuy1811@gmail.com>
2017-07-18 16:57:58 +0700

    fbmsg

--- fbmsg/index.js
+++ fbmsg/index.js
@@ -1,7 +1,7 @@
 exports.handler = function (req, res) {
 	const crypto = require('crypto');
 	const secret = '2798cdb5e2d5167a3c55d62222f79250';
-	console.log(req.body);
+	console.log(JSON.stringify(req.body));
 	console.log(req.headers['x-hub-signature']);
 	if (req.headers['x-hub-signature'] !== 'sha1=fe4196c7ee2bde02f5a8a9263a6007d6c10bf761') {
 		res.status(200).send('Invalid authentication credentials');
--- fbmsg/test.js
+++ fbmsg/test.js
@@ -1,6 +1,7 @@
-var crypto = require('crypto')
-  , text = 'I love cupcakes'
-  , key = 'abcdeg'
-  , hash
-
-hash = crypto.createHmac('sha1', key).update(text).digest('hex')
+const crypto = require('crypto');
+const key = '2798cdb5e2d5167a3c55d62222f79250';
+const res = '';
+const text = 
+res = res.substr(5);
+hash = crypto.createHmac('sha1', key).update(text).digest('hex');
+console.log(hash === res)
