7ed3b8ee339edd2bce9c25a7f5ce653ac35a070e
HuyJokezy <nqhuy1811@gmail.com>
2017-07-19 15:33:54 +0700

    fbmsg

--- fbmsg/index.js
+++ fbmsg/index.js
@@ -20,8 +20,10 @@ exports.handler = function (req, res) {
 			console.log(req.headers['x-hub-signature']);
 			res.status(200).send('Invalid authentication credentials');
 		} else {
+			// Check if incoming message is echo
 			if (req.body.entry[0].messaging[0].message.is_echo !== true) {
 				console.log('Normal Message: ' + req.body.entry[0].messaging[0].message.text);
+				// After timeout duration. Send a waiting message to user
 				var delayMessage = setTimeout(function () {
 					var request = require('request');
 					var token = 'EAAB3JUNaWzgBAJkbBLaCrGgVDoLUfHChBkf0qi8yE9Bg0azTRmMzcpbe2yojIPThR1BxT9HMwcwBlSl2ZBXDGJUS9mFFGedTxGRYKxq9n6ZCj9XqoGDvkr9sVrh3D6tqcjOMBhZC9y8Yxeaix3QTNTZA2r2hATWl8TJ0LCPpBgZDZD';
@@ -45,6 +47,7 @@ exports.handler = function (req, res) {
 						res.status(200).json({});
 					});
 				}, 5000);
+				// Make a request to API.AI
 				var request = require('request');
 				var options = {
 					uri: 'https://api.api.ai/v1/query?v=20150910',
@@ -65,28 +68,17 @@ exports.handler = function (req, res) {
 					}
 				};
 				var callbackApiai = function (error, response, body) {
-					var request = require('request');
-					var token = 'EAAB3JUNaWzgBAJkbBLaCrGgVDoLUfHChBkf0qi8yE9Bg0azTRmMzcpbe2yojIPThR1BxT9HMwcwBlSl2ZBXDGJUS9mFFGedTxGRYKxq9n6ZCj9XqoGDvkr9sVrh3D6tqcjOMBhZC9y8Yxeaix3QTNTZA2r2hATWl8TJ0LCPpBgZDZD';
-					var options = {
-						uri: 'https://graph.facebook.com/v2.6/me/messages?access_token=' + token,
-						method: 'POST',
-						json: true,
-						headers: {
-							'Content-Type': 'application/json'
-						},
-						body: {
-							'recipient': {
-								'id': req.body.entry[0].messaging[0].sender.id
-							},
-							'message': {
-								'text': body.result.fulfillment.speech
-							}
-						}
-					};
-					request(options, function () {
+					var callbackRespond = function () {
 						res.status(200).json({});
 						clearTimeout(delayMessage);
-					});
+					};
+					if (body.result.metadata.webhookUsed === 'true' || body.result.metadata.webhookForSlotFillingUsed === 'true') {
+						res.status(200).json({});
+					} else {
+						for (let i = 0; i < body.result.fulfillment.messages.length; i++) {
+							respond(body.result.fulfillment.messages[i], req.body.entry[0].messaging[0].sender.id, callbackRespond);
+						}
+					}
 				};
 				request(options, callbackApiai);
 			} else {
@@ -118,4 +110,92 @@ function unicodeEscape (str) {
       return '\\u' + ('0000' + (escape.charCodeAt()).toString(16).toLowerCase()).slice(-4);
     }
   });
+};
+
+// Send responses to Facebook
+function respond (messageFromApiAi, recipientId, callbackRespond) {
+	var messageToFb = {};
+	switch (messageFromApiAi.type) {
+		// Text message
+		case 0:
+			messageToFb = {
+				'text': messageFromApiAi.speech
+			};
+			break;
+		// Card
+		case 1:
+			messageToFb = {
+				'atttachment': {
+					'type': 'template',
+					'payload': {
+						'template_type': 'generic',
+						'elements': [
+							{
+								'title': messageFromApiAi.title,
+								'subtitle': messageFromApiAi.subtitle,
+								'image_url': messageFromApiAi.imageUrl,
+								'buttons': [
+								]
+							}
+						]
+					}
+				}
+			};
+			for (let i = 0; i < messageFromApiAi.buttons.length; i++) {
+				messageToFb.atttachment.payload.elements[0].buttons.push({
+					'content_type': (messageFromApiAi.buttons[i].postback.substr(0,3) === 'http') ? 'web_url' : 'postback',
+					'title': messageFromApiAi.buttons[i].text,
+					(messageFromApiAi.buttons[i].postback.substr(0,3) === 'http') ? {'url': messageFromApiAi.buttons[i].postback} : {'payload': messageFromApiAi.buttons[i].postback}
+				});
+			}
+			break;
+		// Quick replies
+		case 2:
+			messageToFb = {
+				'text': messageFromApiAi.title,
+				'quick_replies': []
+			};
+			for (let i = 0; i < messageFromApiAi.replies.length; i++) {
+				messageToFb.quick_replies.push({
+					'content_type': 'text',
+					'title': messageFromApiAi.replies[i],
+					'payload': messageFromApiAi.replies[i]
+				});
+			}
+			break;
+		// Image
+		case 3:
+			messageToFb = {
+				'atttachment': {
+					'type': 'image',
+					'payload': {
+						'url': messageFromApiAi.imageUrl
+					}
+				}
+			};
+			break;
+		// Custom payload
+		case 4:
+			messageToFb = messageFromApiAi.payload;
+			break;
+		default:
+
+	}
+	var request = require('request');
+	var token = 'EAAB3JUNaWzgBAJkbBLaCrGgVDoLUfHChBkf0qi8yE9Bg0azTRmMzcpbe2yojIPThR1BxT9HMwcwBlSl2ZBXDGJUS9mFFGedTxGRYKxq9n6ZCj9XqoGDvkr9sVrh3D6tqcjOMBhZC9y8Yxeaix3QTNTZA2r2hATWl8TJ0LCPpBgZDZD';
+	var options = {
+		uri: 'https://graph.facebook.com/v2.6/me/messages?access_token=' + token,
+		method: 'POST',
+		json: true,
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: {
+			'recipient': {
+				'id': recipientId
+			},
+			'message': messageToFb
+		}
+	};
+	request(options, callbackRespond);
 };
