5546af497e0d9712b77b78d210770ac4b92412df
HuyJokezy <nqhuy1811@gmail.com>
2017-07-19 17:23:12 +0700

    fbmsg

--- fbmsg/index.js
+++ fbmsg/index.js
@@ -21,6 +21,7 @@ exports.handler = function (req, res) {
 			res.status(200).send('Invalid authentication credentials');
 		} else {
 			// console.log(JSON.stringify(req.body));
+			res.status(200).json({});
 			setTimeout(function () {
 				res.status(200).json({});
 			}, 20000);
